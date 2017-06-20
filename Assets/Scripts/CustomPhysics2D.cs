790091e7953ffe4e054586938aaa8f37c62902e2
Ryan Andersen <ryan.andersen05@gmail.com>
2017-06-19 22:53:20 -0400

    Updated Custom Physics.
    Can now choose the direction of gravity

--- Assets/Scripts/CustomPhysics2D.cs
+++ Assets/Scripts/CustomPhysics2D.cs
@@ -9,12 +9,15 @@ using UnityEngine;
 public class CustomPhysics2D : MonoBehaviour {
     #region main variables
     public const float GRAVITY = 9.8f;
+    
     public float gravityScale = 1.0f;
-
-    public Vector2 velocity;
-
+    [Tooltip("The maximum velocity that will be applied by the gravity force. If the speed is greater, gravity will not be applied")]
+    public float terminalVelocity = 10.0f;
+    [Tooltip("This is the direction of the gravity force that will be applied to this object. Will always update to a unit vector")]
     [SerializeField]
     private Vector2 gravityDirection = Vector2.down;
+    [HideInInspector]
+    public Vector2 velocity;
     #endregion main variables
 
     #region monobehaviour methods
@@ -26,8 +29,23 @@ public class CustomPhysics2D : MonoBehaviour {
     private void Update()
     {
         updateVelocityGravity();
+        updatePositionFromVelocity();
+    }
+
+    private void OnValidate()
+    {
+        setGravityDirection(gravityDirection);
+        if (terminalVelocity < 0) terminalVelocity = 0;
     }
     #endregion monobehaviour methods
+
+    private void updatePositionFromVelocity()
+    {
+        print(velocity);
+        Vector2 originalPosition = new Vector2(transform.position.x, transform.position.y);
+        originalPosition += velocity * Time.deltaTime;
+        transform.position = new Vector3(originalPosition.x, originalPosition.y, transform.position.z);
+    }
 
     public void setGravityDirection(Vector2 direction)
     {
@@ -39,6 +57,18 @@ public class CustomPhysics2D : MonoBehaviour {
     /// </summary>
     private void updateVelocityGravity()
     {
+        Vector2 compareVec = gravityDirection * terminalVelocity;
+        float scale = Mathf.Sign(compareVec.x);
 
+        if (scale * compareVec.x > scale * velocity.x)
+        {
+            velocity.x = Mathf.MoveTowards(velocity.x, gravityDirection.x * terminalVelocity, Mathf.Abs(Time.deltaTime * gravityScale * gravityDirection.x * GRAVITY));
+        }
+        scale = Mathf.Sign(compareVec.y);
+        if (scale * compareVec.y > scale * velocity.y)
+        {
+            print(gravityDirection.y * terminalVelocity);
+            velocity.y = Mathf.MoveTowards(velocity.y, gravityDirection.y * terminalVelocity, Mathf.Abs(Time.deltaTime * gravityScale * gravityDirection.y * GRAVITY));
+        }
     }
 }
