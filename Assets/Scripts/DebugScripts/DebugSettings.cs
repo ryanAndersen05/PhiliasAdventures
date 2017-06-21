bdfb456377d4a102b6363925ce54d14fab7ab2a7
Ryan Andersen <ryan.andersen05@gmail.com>
2017-06-20 22:16:35 -0400

    Basic Debug script added

---
+++ Assets/Scripts/DebugScripts/DebugSettings.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugSettings : MonoBehaviour {
+    #region static variables
+    private static DebugSettings instance;
+
+    public static DebugSettings Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = GameObject.FindObjectOfType<DebugSettings>();
+            }
+            return instance;
+        }
+    }
+    #endregion static variables
+
+    #region main variables
+    public bool displayColliderRays = false;
+    #endregion main variables
+
+    #region monobehaviour methods
+    private void Awake()
+    {
+        instance = this;
+    }
+    #endregion monobehaviouir methods
+
+
+}
