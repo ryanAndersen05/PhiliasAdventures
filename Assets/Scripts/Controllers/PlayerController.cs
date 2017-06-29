227cc251d955c6090559f86dce021921ea8631ac
Ryan Andersen <randersen3@gatech.edu>
2017-06-29 02:31:56 -0400

    controller input script has been updated

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -33,20 +33,102 @@ public class PlayerController : MonoBehaviour {
 
     private void Start()
     {
+        controllerDictionary.Add(MOVEMENT, new ControlInfo { isAxis = true });
     }
 
     private void Update()
     {
-        
+        updateAllInputs();
     }
     #endregion monobehaviour methods
 
+    private void updateAllInputs()
+    {
+        Dictionary<string, ControlInfo> updateDictionary = new Dictionary<string, ControlInfo>();
+        ControlInfo controlInfo;
+        foreach (string key in controllerDictionary.Keys)
+        {
+            controlInfo = controllerDictionary[key];
+            if (controlInfo.isAxis)
+            {
+                controlInfo.axisValue = Input.GetAxis(key);
+                controlInfo.axisValueRaw = Input.GetAxisRaw(key);
+            }
+            else
+            {
+                controlInfo.isDown = Input.GetButtonDown(key);
+                controlInfo.isHeld = Input.GetButton(key);
+                controlInfo.isReleased = Input.GetButtonUp(key);
+            }
+        }
+    }
+
+    public bool isDown(string buttonName)
+    {
+        if (controllerDictionary.ContainsKey(buttonName))
+        {
+            Debug.LogWarning("There is no button input set as " + buttonName);
+            return false;
+        }
+        if (controllerDictionary[buttonName].isAxis) Debug.LogWarning("This controller input is set as an axis. Button inputs will not be updated by default");
+
+        return controllerDictionary[buttonName].isDown;
+    }
+
+    public bool isHeld(string buttonName)
+    {
+        if (controllerDictionary.ContainsKey(buttonName))
+        {
+            Debug.LogWarning("There is no button input set as " + buttonName);
+            return false;
+        }
+        if (controllerDictionary[buttonName].isAxis) Debug.LogWarning("This controller input is set as an axis. Button inputs will not be updated by default");
+
+        return controllerDictionary[buttonName].isHeld;
+    }
+
+    public bool isReleased(string buttonName)
+    {
+        if (controllerDictionary.ContainsKey(buttonName))
+        {
+            Debug.LogWarning("There is no button input set as " + buttonName);
+            return false;
+        }
+        if (controllerDictionary[buttonName].isAxis) Debug.LogWarning("This controller input is set as an axis. Button inputs will not be updated by default");
+
+        return controllerDictionary[buttonName].isReleased;
+    }
+
+    public float getAxis(string axisName)
+    {
+        if (!controllerDictionary.ContainsKey(axisName))
+        {
+            Debug.LogWarning("There is no input set as " + axisName);
+            return 0;
+        }
+        if (!controllerDictionary[axisName].isAxis) Debug.LogWarning("This controller input is not set as an axis. By default, axis values will not be updated");
+        return controllerDictionary[axisName].axisValue;
+    }
+
+    public float getAxisRaw(string axisName)
+    {
+        if (!controllerDictionary.ContainsKey(axisName))
+        {
+            Debug.LogWarning("There is no input set as " + axisName);
+            return 0;
+        }
+        if (!controllerDictionary[axisName].isAxis) Debug.LogWarning("This controller is not set as an axis. By default, axis values will not be updated");
+        return controllerDictionary[axisName].axisValueRaw;
+    }
+
     private struct ControlInfo
     {
+        public bool isAxis;
         public bool isDown;
         public bool isHeld;
         public bool isReleased;
-        public float axis;
+        public float axisValue;
+        public float axisValueRaw;
         public float bufferButtonPress;
     }
 }
