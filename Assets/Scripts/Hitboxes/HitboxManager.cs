49f1f2f2bfda6977658666e772c6125e4585d8c5
Ryan Andersen <ryan.andersen05@gmail.com>
2017-07-05 23:06:23 -0400

    some hitbox script started

---
+++ Assets/Scripts/Hitboxes/HitboxManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Since we will be using multiple hitboxes in some animations, it will be important to have a manager so that we are not
+/// unecessarily attacking the same target with a different hit box from the same attack. This hitbox manager will keep track of all active
+/// hit boxes in an animation or object
+/// </summary>
+public class HitboxManager : MonoBehaviour {
+    public Hitbox[] availableHitboxes;
+}
---
+++ Assets/Scripts/Hitboxes/Hurtbox.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Hurtbox : MonoBehaviour {
+
+    private Rigidbody2D unityRigid;
+
+
+    #region monobehaviour methods
+    private void OnValidate()
+    {
+        
+    }
+
+
+    #endregion monobehaviour methods
+
+
+}
