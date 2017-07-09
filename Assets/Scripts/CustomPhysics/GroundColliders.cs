c003a39b4414fb90f77cf94dc72b33374d63153d
Ryan Andersen <ryan.andersen05@gmail.com>
2017-07-09 12:50:03 -0400

    Added a new ground collider script
    Should be able to calculate the proper height of the player with this script once it is implemented, even on non flat surfaces

---
+++ Assets/Scripts/CustomPhysics/GroundColliders.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class is strictly for ground colliders. This will provide the proper height that the player should be at when they are touching the ground
+/// </summary>
+public class GroundColliders : MonoBehaviour {
+    #region enum values
+    public enum GroundType
+    {
+        WALL,
+        FLAT_GROUND,
+        ANGLED_GROUND,
+        ONE_WAY_PLATFORM
+    }
+    #endregion enum values
+
+    #region main variables
+    public GroundType groundType;
+    #endregion main variables
+
+
+    #region monobehaviour methods
+    private void OnValidate()
+    {
+        switch (groundType)
+        {
+            case GroundType.WALL:
+
+                break;
+            case GroundType.ANGLED_GROUND:
+            case GroundType.FLAT_GROUND:
+            case GroundType.ONE_WAY_PLATFORM:
+
+                break;
+        }
+    }
+    #endregion monobehaviour methods
+
+    
+}
