beccc99af993b5a2799d041d1fa057fc61910a05
Ryan Andersen <randersen3@gatech.edu>
2017-07-09 14:07:17 -0400

    Can now choose the type of Ground collider
    Will also adjust the Collision type if it does not match the correct
    type

--- Assets/Scripts/CustomPhysics/GroundColliders.cs
+++ Assets/Scripts/CustomPhysics/GroundColliders.cs
@@ -24,19 +24,42 @@ public class GroundColliders : MonoBehaviour {
     #region monobehaviour methods
     private void OnValidate()
     {
+        Collider2D[] colliders = GetComponents<Collider2D>();
+        if (colliders.Length > 1)
+        {
+            for (int i = 1; i < colliders.Length; i++)
+            {
+                StartCoroutine(DestroyOnValidate(colliders[i]));//We really only want to focus on one collider per ground tile. Destroy all others if they have been added
+            }
+        }
+        Collider2D collider = GetComponent<Collider2D>();
         switch (groundType)
         {
             case GroundType.WALL:
-
+                if (!(collider is BoxCollider2D))
+                {
+                    StartCoroutine(DestroyOnValidate(collider));
+                    this.gameObject.AddComponent<BoxCollider2D>();
+                }
                 break;
             case GroundType.ANGLED_GROUND:
             case GroundType.FLAT_GROUND:
             case GroundType.ONE_WAY_PLATFORM:
-
+                if (!(collider is EdgeCollider2D))
+                {
+                    StartCoroutine(DestroyOnValidate(collider));
+                    this.gameObject.AddComponent<EdgeCollider2D>();
+                }
                 break;
         }
     }
     #endregion monobehaviour methods
+
+    private IEnumerator DestroyOnValidate(Collider2D obj)
+    {
+        yield return new WaitForSeconds(.001f);
+        DestroyImmediate(obj);
+    }
 
     
 }
