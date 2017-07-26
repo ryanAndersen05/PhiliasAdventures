43350f1970de93874a4448b1cbad6c68f80a804f
Ryan Andersen <ryan.andersen05@gmail.com>
2017-07-25 22:07:41 -0400

    animations updated (Still need scripts to make it work)

---
+++ Assets/Scripts/OverseerScripts/GameOverseer.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class is used to hold the general information that is present in the current game
+/// </summary>
+public class GameOverseer : MonoBehaviour {
+
+}
---
+++ Assets/Scripts/OverseerScripts/GameSettings.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// This class holds the general game settings that are used  throughout the game
+/// </summary>
+public class GameSettings : MonoBehaviour {
+    #region static variables
+    public static GameSettings Instance
+    {
+        get
+        {
+            if (!instance)
+            {
+                instance = GameObject.FindObjectOfType<GameSettings>();
+            }
+            return instance;
+        }
+    }
+
+    private static GameSettings instance;
+    #endregion static variables
+
+    #region main variables
+
+    #endregion main variables
+
+
+}
